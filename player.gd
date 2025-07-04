39f43f661ed6d23f97063ce5bf5c142c813b89c8
Clemente Gonzales <cjaygonzales117@gmail.com>
2025-07-04 13:58:42 +0100

    added further animations

--- player.gd
+++ player.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	# Handle Dash
 	if Input.is_action_just_pressed("dash_movement_right"):
 		velocity.x = JUMP_VELOCITY * -3.5
+		#animated_sprite_2d.play("roll")
 	
 	# Handle Dash
 	if Input.is_action_just_pressed("dash_movement_left"):
@@ -23,7 +28,25 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	
 	var direction := Input.get_axis("left_movement", "right_movement")
+	
+	# adding the animated sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+	# play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("running")
+	else:
+		animated_sprite_2d.play("roll")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
